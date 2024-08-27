f4dde276041ee148a908d4cec3acf2af8cd5762a
JaninduTharuka <janindutharuka2001@gmail.com>
2024-08-27 23:01:00 +0530

    Password encryption-level2

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+    .then(() => console.log('Connected to Database'))
+    .catch(err => console.error('Error connecting to Database...:', err));
+
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const secret = 'ajsdhha6253akjdak$hahsd5798kjhfjs8787^&^*ajfksdkfjslka5swr00we9r-fdsl';
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+const User = mongoose.model('User', userSchema);
+
+
+app.route('/')
+    .get((req, res) => {
+        res.render('home');
+    });
+
+app.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save()
+            .then(() => res.render('secrets'))
+            .catch(err => res.send(err));
+    });
+
+app.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req,res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({ email: username })
+            .then(foundUser => {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render('secrets');
+                    } else {
+                        res.send('Incorrect password');
+                    }
+                } else {
+                    res.send('User not found');
+                }
+            })
+            .catch(err => res.send(err));
+    })
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
